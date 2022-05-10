52a937e45472b505f71d2af9dafae61df45b0d9b
Taiga Miura <miura.t84@gmail.com>
2022-05-10 15:59:39 +0900

    memo app

---
+++ src/components/App.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, useState, FC, useCallback } from "react";
+import styled from "styled-components";
+import { MemoList } from "./MemoList"; 
+
+export const App: FC = () => {
+    const [text, setText] = useState<string>("");
+    const [memos, setMemos] = useState<string[]>([]);
+
+    const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
+
+    const onClickAdd = () => {
+        const newMemos = [...memos];
+        newMemos.push(text);
+        setMemos(newMemos);
+        setText("");
+    };
+
+    const onClickDelete = useCallback((index: number) => {
+        const newMemos = [...memos];
+        newMemos.splice(index, 1);
+        setMemos(newMemos);
+    }, [memos]);
+
+    return(
+        <div>
+            <h1>Memo App</h1>
+            <input type="text" value={text} onChange={onChangeText} />
+            <SButton onClick={onClickAdd}>追加</SButton>
+            <MemoList memos={memos} onClickDelete={onClickDelete} />
+        </div>
+    );
+};
+
+const SButton = styled.button`
+    margin-left: 16px;
+`;
